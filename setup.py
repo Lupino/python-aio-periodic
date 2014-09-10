35046e882fe680837508be4ef56a7240264d9b9b
Ubasic <lmjubuntu@gmail.com>
2014-09-10 19:56:24 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'aio_periodic',
+]
+
+requires = ['aio_periodic']
+
+setup(
+    name='aio_periodic',
+    version='0.0.1',
+    description='The periodic task system client for python3 base on asyncio',
+    author='Li Meng Jun',
+    author_email='lmjubuntu@gmail.com',
+    url='',
+    packages=packages,
+    package_dir={'aio_periodic': 'aio_periodic'},
+    include_package_data=True,
+    install_requires=requires,
+)
