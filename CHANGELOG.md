c61a5ed50297a63e7c2ef8b7314a9c3a0513a15e
Ubasic <lmjubuntu@gmail.com>
2014-11-21 17:23:49 +0800

    set version 0.1.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 0.1.5
+- update procotol name
+
 ## 0.1.4
 - update the status api data format
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = ['aio_periodic']
 
 setup(
     name='aio_periodic',
-    version='0.1.4',
+    version='0.1.5',
     description='The periodic task system client for python3 base on asyncio',
     author='Li Meng Jun',
     author_email='lmjubuntu@gmail.com',
