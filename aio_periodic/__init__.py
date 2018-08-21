54c7ee4ba9f3cd371ae5c10d1313c7ba04d0e7d6
Lupino <lmjubuntu@gmail.com>
2018-08-21 10:20:39 +0800

    update export

--- aio_periodic/__init__.py
+++ aio_periodic/__init__.py
@@ -1,5 +1,7 @@
 from .client import Client
 from .worker import Worker
 from .pool import Pool
+from .transport import open_connection
+from .types.job import Job
 
-__all__ = ['Client', 'Worker', "Pool"]
+__all__ = ['Client', 'Worker', "Pool", "open_connection", "Job"]
