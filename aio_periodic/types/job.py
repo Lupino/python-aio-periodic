95af90246d4b80f3c3e532b98b3cf339a481fcf6
Lupino <lmjubuntu@gmail.com>
2018-08-22 19:52:38 +0800

    add timeout support

--- aio_periodic/types/job.py
+++ aio_periodic/types/job.py
@@ -2,20 +2,29 @@ from .utils import encode_str8, encode_str32, encode_int32, encode_int64, \
         decode_int8, decode_int16, decode_int32, decode_int64, encode_int8
 
 class Job(object):
-    def __init__(self, func, name, workload = b'', sched_at = 0, count = 0):
+    def __init__(self, func, name, workload = b'', sched_at = 0, count = 0, timeout = 0):
         self.func = func
         self.name = name
         self.workload = workload
         self.sched_at = sched_at
         self.count = count
+        self.timeout = timeout
 
     def pack(self):
         buf = b''
         ver = 0
+        if self.count > 0 && self.timeout > 0:
+            ver = 3
+        if self.timeout > 0:
+            ver = 2
         if self.count > 0:
             ver = 1
         if ver == 1:
             buf = encode_int32(self.count)
+        if ver == 2:
+            buf = encode_int32(self.timeout)
+        if ver == 3:
+            buf = encode_int32(self.count) + encode_int32(self.timeout)
         return b''.join([
             encode_str8(self.func),
             encode_str8(self.name),
@@ -53,6 +62,11 @@ class Job(object):
 
         if ver == 1:
             job['count'] = decode_int32(payload[0:4])
+        if ver == 2:
+            job['timeout'] = decode_int32(payload[0:4])
+        if ver == 3:
+            job['count'] = decode_int32(payload[0:4])
+            job['timeout'] = decode_int32(payload[4:8])
 
         return cls(**job)
 
