0374d0d21f0ba11edbd73337cd66bfb008865d35
Lupino <lmjubuntu@gmail.com>
2019-05-18 07:26:00 +0800

    set version 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = ['aio_periodic']
 
 setup(
     name='aio_periodic',
-    version='0.1.8',
+    version='0.2.0',
     description='The periodic task system client for python3 base on asyncio',
     author='Li Meng Jun',
     author_email='lmjubuntu@gmail.com',
