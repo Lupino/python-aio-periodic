b74e58e5d10c386a42173e050f6c1dab84db7dd4
Lupino <lmjubuntu@gmail.com>
2020-10-23 14:42:55 +0800

    update examples

--- examples/echo_client.py
+++ examples/echo_client.py
@@ -2,37 +2,41 @@ from aio_periodic import open_connection, Client, Job
 import asyncio
 from time import time
 
+
 async def main(loop):
     client = Client(loop)
     await client.connect(open_connection, 'unix:///tmp/periodic.sock')
 
     job = Job(func='echo', name='test_echo')
-    job2 = Job(func='echo', name='test_echo2', sched_at=int(time()) + 10000, timeout=10)
+    job2 = Job(func='echo',
+               name='test_echo2',
+               sched_at=int(time()) + 10000,
+               timeout=10)
     job3 = Job(func='echo', name='test_echo3', sched_at=int(time()) + 1)
     job4 = Job(func='echo2', name='test_echo3', sched_at=int(time()) + 1)
     job5 = Job(func='echo_later', name='test_echo_later')
     print(await client.ping())
-    print(await client.submit_job(job))
-    print(await client.run_job(job))
+    print(await client.submit_job(job=job))
+    print(await client.run_job(job=job))
 
-    print(await client.submit_job(job2))
+    print(await client.submit_job(job=job2))
     print(await client.remove_job('echo', 'test_echo2'))
 
-    print(await client.run_job(job3))
+    print(await client.run_job(job=job3))
 
-    print(await client.submit_job(job4))
+    print(await client.submit_job(job=job4))
     print(await client.status())
     print(await client.drop_func('echo2'))
     print(await client.status())
-    print(await client.run_job(job5))
+    print(await client.run_job(job=job5))
     try:
-        print(await client.run_job(job4))
+        print(await client.run_job(job=job4))
     except Exception as e:
         print(e)
 
     for i in range(0, 100):
         job6 = Job(func='test_lock', name='test_lock_' + str(i))
-        await client.submit_job(job6)
+        await client.submit_job(job=job6)
 
 
 loop = asyncio.get_event_loop()
--- examples/echo_worker.py
+++ examples/echo_worker.py
@@ -1,9 +1,11 @@
 from aio_periodic import open_connection, Worker
 import asyncio
 
+
 async def echo(job):
     print(job.name)
     await job.done(job.name)
+
 
 async def echo_later(job):
     print(job.name, job.payload.count)
@@ -12,12 +14,14 @@ async def echo_later(job):
     else:
         await job.sched_later(1, 1)
 
+
 async def test_lock(job):
     async def do_lock():
         await asyncio.sleep(1)
         await echo(job)
 
     await job.with_lock('test', 2, do_lock)
+
 
 async def main(loop):
     worker = Worker(loop)
@@ -26,7 +30,8 @@ async def main(loop):
     await worker.add_func('echo', echo)
     await worker.add_func('echo_later', echo_later)
     await worker.add_func('test_lock', test_lock)
-    await worker.work(10)
+    worker.work(10)
+
 
 loop = asyncio.get_event_loop()
 
