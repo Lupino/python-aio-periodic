859bf83b69a889363da6812dbccacc8701e22070
Lupino <lmjubuntu@gmail.com>
2020-10-23 14:44:14 +0800

    add cluster example

---
+++ examples/echo_client_cluster.py
@@ -0,0 +1,43 @@
+from aio_periodic import open_connection, Job, ClientCluster
+import asyncio
+from time import time
+
+
+async def main(loop):
+    client = ClientCluster(['tcp://:5000', 'tcp://:5001'], loop=loop)
+    await client.connect(open_connection)
+
+    job = Job(func='echo', name='test_echo')
+    job2 = Job(func='echo',
+               name='test_echo2',
+               sched_at=int(time()) + 10000,
+               timeout=10)
+    job3 = Job(func='echo', name='test_echo3', sched_at=int(time()) + 1)
+    job4 = Job(func='echo2', name='test_echo3', sched_at=int(time()) + 1)
+    job5 = Job(func='echo_later', name='test_echo_later')
+    print(await client.submit_job(job=job))
+    print(await client.run_job(job=job))
+
+    print(await client.submit_job(job=job2))
+    print(await client.remove_job('echo', 'test_echo2'))
+
+    print(await client.run_job(job=job3))
+
+    print(await client.submit_job(job=job4))
+    print(await client.status())
+    print(await client.drop_func('echo2'))
+    print(await client.status())
+    print(await client.run_job(job=job5))
+    try:
+        print(await client.run_job(job=job4))
+    except Exception as e:
+        print(e)
+
+    for i in range(0, 100):
+        job6 = Job(func='test_lock', name='test_lock_' + str(i))
+        await client.submit_job(job=job6)
+
+
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(main(loop))
---
+++ examples/echo_worker_cluster.py
@@ -0,0 +1,40 @@
+from aio_periodic import open_connection, WorkerCluster
+import asyncio
+
+worker = WorkerCluster(['tcp://:5000', 'tcp://:5001'])
+
+
+@worker.func('echo')
+async def echo(job):
+    print(job.name)
+    await job.done(job.name)
+
+
+@worker.func('echo_later')
+async def echo_later(job):
+    print(job.name, job.payload.count)
+    if job.payload.count > 3:
+        await job.done(job.name)
+    else:
+        await job.sched_later(1, 1)
+
+
+@worker.func('test_lock')
+async def test_lock(job):
+    async def do_lock():
+        await asyncio.sleep(1)
+        await echo(job)
+
+    await job.with_lock('test', 2, do_lock)
+
+
+async def main(loop):
+    await worker.connect(open_connection)
+    worker.work(10)
+
+
+loop = asyncio.get_event_loop()
+
+loop.create_task(main(loop))
+
+loop.run_forever()
