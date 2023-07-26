771bb2977264d4819b815fa283682dc6874ea67f
Lupino <lmjubuntu@gmail.com>
2023-07-26 10:35:52 +0800

    improve open_connection

--- aio_periodic/transport.py
+++ aio_periodic/transport.py
@@ -7,11 +7,8 @@ logger = logging.getLogger('aio_periodic.transport')
 async def open_connection(entrypoint):
     logger.info("open connection: " + entrypoint)
     if entrypoint.startswith('unix://'):
-        reader, writer = await asyncio.open_unix_connection(
-            entrypoint.split('://')[1])
+        return await asyncio.open_unix_connection(entrypoint.split('://')[1])
     else:
         host_port = entrypoint.split('://')[1].split(':')
-        reader, writer = await asyncio.open_connection(host_port[0],
-                                                       host_port[1])
+        return await asyncio.open_connection(host_port[0], host_port[1])
 
-    return reader, writer
