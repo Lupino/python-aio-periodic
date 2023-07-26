cdc86e358397fce413ffb23bbd95e0745ebc5f2c
Lupino <lmjubuntu@gmail.com>
2023-07-26 10:38:31 +0800

    remove empty end line of transport.py

--- aio_periodic/transport.py
+++ aio_periodic/transport.py
@@ -11,4 +11,3 @@ async def open_connection(entrypoint):
     else:
         host_port = entrypoint.split('://')[1].split(':')
         return await asyncio.open_connection(host_port[0], host_port[1])
-
